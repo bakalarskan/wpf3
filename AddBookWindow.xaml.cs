290b49bd7fe8bb7bb8bc5d411b6f2056209870dc
Natalia Bakalarska <natalia.bakalarska@interia.pl>
2026-05-30 13:06:15 +0200

    browse save i cancel działają

--- AddBookWindow.xaml.cs
+++ AddBookWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace libraria
 {
@@ -19,9 +20,44 @@ namespace libraria
     /// </summary>
     public partial class AddBookWindow : Window
     {
+        public Book book { get; private set; }
         public AddBookWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image files (*.jpg, *.jpeg, *.png) | *.jpg; *.jpeg; *.png";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string selectedImagePath = openFileDialog.FileName;
+                BitmapImage bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.UriSource = new Uri(selectedImagePath);
+                bitmap.EndInit();
+                CoverImage.Source = bitmap;
+            }
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+            this.Close();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            book = new Book
+            {
+                Title = TitleBox.Text,
+                Author = AuthorBox.Text,
+                Description = DescriptionBox.Text,
+                CoverImage = CoverImage.Source
+            };
+            this.DialogResult = true;
+            this.Close();
         }
     }
 }
